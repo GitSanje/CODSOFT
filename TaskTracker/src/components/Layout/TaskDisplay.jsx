67b4b75ce36d576a4849073b542b37552d33d25e
Sanjay Karki <karkisanjay2002@gmail.com>
2024-07-24 22:20:23 +0545

    task grid layout

---
+++ TaskTracker/src/components/Layout/TaskDisplay.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const TaskDisplay = () => {
+  return (
+    <>
+    <div className="bg-white rounded shadow-sm">
+      <div className="text-black text-start text-md font-semibold p-5">
+         Develop New E-reader
+      </div>
+    </div>
+      
+    </>
+  )
+}
+
+export default TaskDisplay
